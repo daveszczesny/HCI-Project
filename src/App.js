e6eca6603b48fb48c6323af38262a5fe7c8068b9
Dave <D.szczesny1@universityofgalway.ie>
2023-10-16 17:27:16 +0100

    Added routing

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ReactDom from 'react-dom/client';
 import Home from './pages/home/Home.js';
+import About from './pages/about/About.js'
 
 function App()
 {
   return(
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}>
-          <Route index element={<Home />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
