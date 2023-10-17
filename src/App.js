a4d52bbfd869c10c61fb31e61b76e80eb110e49f
Dave <D.szczesny1@universityofgalway.ie>
2023-10-17 14:31:07 +0100

    added firebase to project

--- src/App.js
+++ src/App.js
@@ -5,9 +5,23 @@ import About from './pages/AboutPage/About.js'
 import FirstLandingPage from './pages/FirstLandingPage/FirstLandingPage.js'
 import LoginPage from './pages/LoginPage/Login';
 
-function App()
-{
-  return(
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKilBqBSec1bE7CBpd0hPhOAcGIHStDO0",
+  authDomain: "hci-mediguardian.firebaseapp.com",
+  projectId: "hci-mediguardian",
+  storageBucket: "hci-mediguardian.appspot.com",
+  messagingSenderId: "791725451126",
+  appId: "1:791725451126:web:fb0bcfe97e0b3b82abfc57"
+};
+
+const appFirebase = initializeApp(firebaseConfig);
+
+
+
+function App() {
+  return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<FirstLandingPage />} />
