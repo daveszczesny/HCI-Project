0d8165a211cc91fc3e9e6d82e316ca71ca122f7d
Dave <D.szczesny1@universityofgalway.ie>
2023-10-17 13:01:35 +0100

    Progress for Login page

--- src/pages/LoginPage/Login.js
+++ src/pages/LoginPage/Login.js
@@ -1,10 +1,19 @@
 import { useState } from "react";
 import './Login.css'
 
+
+const SignUp = () => {
+    console.log("sign up button clicked");
+}
+
+const SignIn = () => {
+    console.log("sign in button clicked");
+}
+
 const LoginPage = () => {
 
-    const [showLoginForm, setShowLoginForm] = useState(true);
-    const [showSignupForm, setShowSignupForm] = useState(false);
+    const [showLoginForm, setShowLoginForm] = useState(false);
+    const [showSignupForm, setShowSignupForm] = useState(true);
 
 
     const switchForm = () => {
@@ -13,16 +22,78 @@ const LoginPage = () => {
         setShowSignupForm(temp);
     }
 
+
+
+
     return (
         <div>
             <div className="login-heading">
-               {showLoginForm? (
-                <h1>Login</h1>
-               ):(
-                <h1>Sign up</h1>
-               )}
+                {showLoginForm ? (
+                    <h1>Login</h1>
+                ) : (
+                    <h1>Sign up</h1>
+                )}
             </div>
 
+
+            <div className={`form-container signup-form ${showSignupForm ? '' : 'alt-view'}`}>
+
+                <form>
+                    <div className="input-container">
+                        <div className="first-name-section">
+                            <input type="text" placeholder=" " />
+                            <label htmlFor="first-name">First Name</label>
+                        </div>
+
+                        <div className="email-section">
+                            <input type="text" placeholder=" " />
+                            <label htmlFor="email">Email</label>
+                        </div>
+
+                        <div className="password-section">
+                            <input type="password" placeholder=" " />
+                            <label htmlFor="password">Password</label>
+                        </div>
+
+                        <div className="signup-button-container">
+                            <button type="submit" onClick={SignUp}>Sign up</button>
+                        </div>
+
+                    </div>
+                </form>
+
+            </div>
+
+            <hr className="separater-line" />
+
+
+            {
+                showLoginForm ? (
+                    <div className="form-container">
+                        <form>
+                            <div className="input-container">
+                                <div className="email-section">
+                                    <input type="text" placeholder=" " />
+                                    <label htmlFor="email">Email</label>
+                                </div>
+
+                                <div className="password-section">
+                                    <input type="password" placeholder=" " />
+                                    <label htmlFor="password">Password</label>
+                                </div>
+
+                                <div className="signup-button-container">
+                                    <button type="submit" onClick={SignIn}>Sign In</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                ) : (
+                    <div className="switch-forms-container">
+                        <button onClick={switchForm}>Sign in instead</button>
+                    </div>
+                )
+            }
 
         </div>
     );
